8d17b336762877ad0ce7508cd3307bbe3af2a1ff
jae-hwan-kim <woghks7859@gmail.com>
2023-10-22 09:09:52 +0900

    FEAT: 콜렉션 안에 콜렉션 데이터까지 출력 해봄

--- ride-the-wave/src/App.js
+++ ride-the-wave/src/App.js
@@ -7,12 +7,14 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const docRef = doc(firestore, "users", "user_id_001");
-      const docSnap = await getDoc(docRef);
+      // const docRef = doc(firestore, "users", "user_id_001");
+      const docRef = doc(firestore, "records", "user_id_001");
+      const docRef2 = doc(docRef, "freestyle", "freestyle");
+      const docSnap = await getDoc(docRef2);
   
       if (docSnap.exists()) {
           console.log("Document data:", docSnap.data());
-          setDocument(docSnap.data().name);
+          setDocument(docSnap.data().start_50);
       } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
