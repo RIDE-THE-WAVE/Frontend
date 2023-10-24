613d45236a6dbbfdacfaf49a5d593da7426a7b3b
jae-hwan-kim <woghks7859@gmail.com>
2023-10-24 18:50:58 +0900

    FEAT: Review 페이지 UI 프레임 완성 #1

--- ride-the-wave/src/component/login/Login.js
+++ ride-the-wave/src/component/login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Login.module.css';
-import logo from './img/logo.png';
+import logo from '../img/logo.png';
 
 function Login() {
     
--- ride-the-wave/src/component/review/Review.js
+++ ride-the-wave/src/component/review/Review.js
@@ -1,41 +1,72 @@
 import React from 'react';
+import logo from '../img/logo.png'; // 나중에 공통이미지는 따로 관리하기
+import styles from './Review.module.css';
 
 function Review() {
-    const divOneStyle = {
-        width: '100%',
-        height: '900px',
-        backgroundColor: '#E5E3E4',
-        color: 'white', 
-    };
-    const divTwoStyle = {
-        width: '100%',
-        height: '500px',
-        backgroundColor: '#5BA199',
-        color: 'white', 
-    };
-    const divThreeStyle = {
-        width: '100%',
-        height: '200px',
-        backgroundColor: '#BBC6C8',
-        color: 'white', 
-    };
-    const divFourStyle = {
-        width: '100%',
-        height: '400px',
-        backgroundColor: '#888888',
-        color: 'white',
-    };
-    const divFiveStyle = {
-        width: '100%',
-        height: '200px',
-        backgroundColor: '#111111',
-        color: 'white',
-    };
   return (
-    <div className="Review" style={divOneStyle}>
-      <h1>Review</h1>
+    <div className={styles.Review}>
+        <div className={styles.item_2}>2
+            Review
+            <div className={styles.item_3}>3
+                <img src={logo} alt="logo" />
+            </div>
+            <div className={styles.item_3}>3
+                <div className={styles.item_4}>4
+                    {/* 제목 박스가 들어갈 예정 */}
+                    <div className={styles.item_5}>5 아무말 대잔치
+                    </div>
+                    <div className={styles.item_5}>5 연필이미지
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={styles.item_2}>2
+          <div className={styles.item_3_2_1}>
+            설명 - 오른쪽 정렬 (24시간 후 자동으로~)
+          </div>
+          <div className={styles.item_3_2_2}>
+                {/* 하단 클릭바 */}
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+                <div className={styles.item_4_2}>4
+                </div>
+              </div>
+          </div>
+        <div className={styles.item_2}>2
+            <div className={styles.item_3_3}>
+              {/* 하단 클릭바 */}
+              <div className={styles.item_4_2}>4
+              </div>
+              <div className={styles.item_4_2}>4
+              </div>
+              <div className={styles.item_4_2}>4
+              </div>
+            </div>
+        </div>
     </div>
-  );
+);
 }
 
 export default Review;
