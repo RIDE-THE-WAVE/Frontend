1eabfc3d585f074a00a4879212918e73a24df0af
jae-hwan-kim <woghks7859@gmail.com>
2023-10-25 18:23:51 +0900

    FEAT: GroupRecords 페이지 UI 프레임 완성 #1

--- ride-the-wave/src/component/records/grouprecords/GroupRecords.js
+++ ride-the-wave/src/component/records/grouprecords/GroupRecords.js
@@ -1,43 +1,166 @@
 import React from 'react';
+import logo from '../../img/logo.png'; // 나중에 공통이미지는 따로 관리하기
+import styles from './GroupRecords.module.css';
+
 
 function GroupRecords() {
-    const divOneStyle = {
-        width: '100%',
-        height: '900px',
-        backgroundColor: '#E5E3E4',
-        color: 'white', 
-    };
-    const divTwoStyle = {
-        width: '100%',
-        height: '500px',
-        backgroundColor: '#5BA199',
-        color: 'white', 
-    };
-    const divThreeStyle = {
-        width: '100%',
-        height: '200px',
-        backgroundColor: '#BBC6C8',
-        color: 'white', 
-    };
-    const divFourStyle = {
-        width: '100%',
-        height: '400px',
-        backgroundColor: '#888888',
-        color: 'white',
-    };
-    const divFiveStyle = {
-        width: '100%',
-        height: '200px',
-        backgroundColor: '#111111',
-        color: 'white',
-    };
-
     return (
-        <div className="GroupRecords" style={divOneStyle}>
-            <h1>GroupRecords</h1>
+        <div className={styles.GroupRecords}>
+            <div className={styles.item_2}>2
+                GroupRecords
+                <div className={styles.item_3}>3
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className={styles.item_3}>3
+                    <div className={styles.item_4}>
+                        {/* 제목 박스가 들어갈 예정 */}
+                        <div className={styles.item_5}>5 돋보기 이미지
+                        </div>
+                        4
+                        <div className={styles.item_5}>5 검색창
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.item_2_2}>
+                <div className={styles.item_3_2}>
+                    토글. 화살표 이미지 + 날짜 3
+                </div>
+                <div className={styles.item_3_2}>
+                    토글. 화살표 이미지 + 시간 3
+                </div>
+                <div className={styles.item_3_2}>
+                    토글. 화살표 이미지 + 영법 3
+                </div>
+            </div>
+            <div className={styles.item_2_3}>
+                  <div className={styles.item_3_3}>3
+                  </div>
+                  <div className={styles.item_3_3}>3
+                  </div>
+                  <div className={styles.item_3_3}>3
+                  </div>
+                  <div className={styles.item_3_3}>3
+                  </div>
+                  <div className={styles.item_3_3}>3
+                  </div>
+            </div>
+            <div className={styles.item_2}>
+                <div className={styles.item_3_4}>
+                  <div className={styles.item_4_2}>4 25
+                  </div>
+                  <div className={styles.item_4_2}>4 50
+                  </div>
+                  <div className={styles.item_4_2}>4 전체
+                  </div>
+                </div>
+            </div>
+            <div className={styles.item_2}>2
+              <div className={styles.item_3_2_2}>
+                    {/* 무한 스크롤 */}
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                    <div className={styles.item_4_2}>
+                        <div className={styles.item_5_5}>5 이름
+                        </div>
+                        <div className={styles.item_5_5}>5 25m
+                        </div>
+                        <div className={styles.item_5_5}>5 50m
+                        </div>
+                    </div>
+                  </div>
+              </div>
+            <div className={styles.item_2}>2
+                <div className={styles.item_3_6}>
+                  {/* 하단 클릭바 */}
+                  <div className={styles.item_4_6}>4
+                  </div>
+                  <div className={styles.item_4_6}>4
+                  </div>
+                  <div className={styles.item_4_6}>4
+                  </div>
+                </div>
+            </div>
         </div>
     );
-
 }
 
 export default GroupRecords;
