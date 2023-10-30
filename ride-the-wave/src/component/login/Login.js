f95c0bffb2d61cc12a96fa52c8cb7b9a353f019f
jae-hwan-kim <woghks7859@gmail.com>
2023-10-31 05:05:52 +0900

    FEAT: 로그인 페이지 css 구현 #5 #6

--- ride-the-wave/src/component/login/Login.js
+++ ride-the-wave/src/component/login/Login.js
@@ -10,20 +10,20 @@ function Login() {
         <div className={styles.Login}>
             <div></div>
             <div className={styles.center_box}>
-                <div className={styles.center_inner_box}>
+                <div className={styles.center_upper_inner_box}>
                     <Link to="/"><img src={logo} alt="logo" /></Link>
                 </div>
-                <div className={styles.center_inner_box}>
+                <div className={styles.center_under_inner_box}>
                     <div className={styles.search_box}>
                         {/* 이름 입력 박스가 들어갈 예정 */}
-                        <div className={styles.put_search_data_section}>5 이름을 입력하세요
-                        </div>
-                        <div className={styles.search_img}>
-                            {/* 버튼 클릭하면 회원 정보 확인 및 권한 부여 후 다음 페이지로 넘어가는 기능 구현 */}
+                        <div className={styles.put_search_data_section}>
+                            <div></div>
+                            <span>이름을 입력하세요.</span>
                             <Link to="/myrecord">
-                                {/* <span> */}
-                                    <img src={search_client} alt="search_client" width='100%'/>
-                                {/* </span> */}
+                                <div className={styles.search_img}>
+                                    {/* 버튼 클릭하면 회원 정보 확인 및 권한 부여 후 다음 페이지로 넘어가는 기능 구현 */}
+                                    <img src={search_client} alt="search_client"/>
+                                </div>
                             </Link>
                         </div>
                     </div>
