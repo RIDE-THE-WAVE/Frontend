a9cd1ff0f55bd05ef7aab77edc7e4b65b6c5e12a
jae-hwan-kim <woghks7859@gmail.com>
2023-11-03 16:39:36 +0900

    FIX: 모달 문구 수정 #8

--- ride-the-wave/src/component/modal/MyRecordModal.js
+++ ride-the-wave/src/component/modal/MyRecordModal.js
@@ -9,6 +9,9 @@ function MyRecordModal() {
             <div>
                 <span>
                 기록 노출을 원하지 않는 경우, 기록 옆 눈 이미지를 눌러주세요.
+                <br/>
+                <br/>
+                * 25m, 50m 함께 적용됩니다.
                 </span>
                 <span><img src={hide} alt="hide"/></span>
             </div>
