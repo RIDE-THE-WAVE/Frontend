29656392f3da7d255ebf724f191e2db357a402f2
jae-hwan-kim <woghks7859@gmail.com>
2023-11-03 11:40:07 +0900

    CHORE: 주석 제거 #8

--- ride-the-wave/src/component/login/Login.js
+++ ride-the-wave/src/component/login/Login.js
@@ -27,14 +27,11 @@ function Login() {
                 </div>
                 <div className={styles.center_bottom_inner_box}>
                     <div className={styles.search_box}>
-                        {/* 이름 입력 박스가 들어갈 예정 */}
                         <div className={styles.put_search_data_section}>
                             <div></div>
                             <input type="text" placeholder="이름을 입력하세요." value={username} onChange={(e) => setUsername(e.target.value)} />
-                            {/* <span>이름을 입력하세요.</span> */}
                             <div onClick={handleLogin}>
                                 <div className={styles.search_img}>
-                                    {/* 버튼 클릭하면 회원 정보 확인 및 권한 부여 후 다음 페이지로 넘어가는 기능 구현 */}
                                     <img src={search_client} alt="search_client"/>
                                 </div>
                             </div>
