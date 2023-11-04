a06a99ed43b0ccb4fc0efaefb2ab6af2f5885919
jae-hwan-kim <woghks7859@gmail.com>
2023-11-04 09:53:23 +0900

    FEAT: 전체 기록페이지 턴, 거리 버튼 클릭시 강조 구현 #8

--- ride-the-wave/src/component/records/grouprecords/GroupRecords.js
+++ ride-the-wave/src/component/records/grouprecords/GroupRecords.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './GroupRecords.module.css';
 import logo from '../../img/logo.png'; // 나중에 공통이미지는 따로 관리하기
 import arrow from '../../img/arrow.png';
@@ -9,6 +9,30 @@ import BottomNav from '../../common/BottomNav';
 
 
 function GroupRecords() {
+    // 개인 기록(마이페이지)와 겹치는 구간이 매우 많음
+    const [activeTurnTab, setActiveTurnTab] = useState('tabEntireTurn');
+    const [activeLengthTab, setActiveLengthTab] = useState('tabEntireLength');
+    // const [activeSide, setActiveSide] = useState(true);
+    // const [activeFlip, setActiveFlip] = useState(true);
+    // const [activeStart, setActiveStart] = useState(true);
+    // const [activeFin, setActiveFin] = useState(true);
+
+    // const toggleActiveSide = () => {
+    //     setActiveSide(!activeSide);
+    // }
+
+    // const toggleActiveFlip = () => {
+    //     setActiveFlip(!activeFlip);
+    // }
+
+    // const toggleActiveStart = () => {
+    //     setActiveStart(!activeStart);
+    // }
+
+    // const toggleActiveFin= () => {
+    //     setActiveFin(!activeFin);
+    // }
+
     return (
         <div className={styles.GroupRecords}>
             <div className={styles.header}>
@@ -23,6 +47,7 @@ function GroupRecords() {
                         <div className={styles.user_search_img}>
                             <img src={search_record} alt="search_record" />
                         </div>
+                        {/* Login 의 입력 받는 창 넣기 */}
                         <div className={styles.user_search}>
                             <span>이름</span>
                         </div>
@@ -49,31 +74,31 @@ function GroupRecords() {
                 </div>
             </div>
             <div className={styles.contents_turn_type}>
-                  <div className={styles.turn_type}>
+                <div className={`${styles.turn_type} ${activeTurnTab === "tabSide" ? styles.turn_type_active : ""}`} onClick={() => setActiveTurnTab("tabSide")}>
                     <span>사이드</span>
-                  </div>
-                  <div className={styles.turn_type}>
+                </div>
+                <div className={`${styles.turn_type} ${activeTurnTab === "tabFlip" ? styles.turn_type_active : ""}`} onClick={() => setActiveTurnTab("tabFlip")}>
                     <span>플립</span>
-                  </div>
-                  <div className={styles.turn_type}>
+                </div>
+                <div className={`${styles.turn_type} ${activeTurnTab === "tabStart" ? styles.turn_type_active : ""}`} onClick={() => setActiveTurnTab("tabStart")}>
                     <span>스타트</span>
-                  </div>
-                  <div className={styles.turn_type}>
+                </div>
+                <div className={`${styles.turn_type} ${activeTurnTab === "tabFin" ? styles.turn_type_active : ""}`} onClick={() => setActiveTurnTab("tabFin")}>
                     <span>오리발</span>
-                  </div>
-                  <div className={styles.turn_type}>
+                </div>
+                <div className={`${styles.turn_type} ${activeTurnTab === "tabEntireTurn" ? styles.turn_type_active : ""}`} onClick={() => setActiveTurnTab("tabEntireTurn")}>
                     <span>전체</span>
-                  </div>
+                </div>
             </div>
             <div className={styles.contents_length_option_box}>
                 <div className={styles.length_option_inner_box}>
-                    <div className={styles.length_option}>
+                    <div className={`${styles.length_option} ${activeLengthTab === "tabHalf" ? styles.length_option_active : ""}`} onClick={() => setActiveLengthTab("tabHalf")}>
                         <span>25</span>
                     </div>
-                    <div className={styles.length_option}>
+                    <div className={`${styles.length_option} ${activeLengthTab === "tabFull" ? styles.length_option_active : ""}`} onClick={() => setActiveLengthTab("tabFull")}>
                         <span>50</span>
                     </div>
-                    <div className={styles.length_option}>
+                    <div className={`${styles.length_option} ${activeLengthTab === "tabEntireLength" ? styles.length_option_active : ""}`} onClick={() => setActiveLengthTab("tabEntireLength")}>
                         <span>전체</span>
                     </div>
                 </div>
