706d6d52e6391025234f461e00055e9bd82d6707
jae-hwan-kim <woghks7859@gmail.com>
2023-11-06 10:04:21 +0900

    FIX: 로딩 페이지 수정 #11

--- ride-the-wave/src/App.js
+++ ride-the-wave/src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Loading from './component/common/Loading';
 
 // 전처리 때 하지 않고 필요할 때만 불러오기 때문에 현재 상황에서는 효과적이다.
 const Login = lazy(() => import('./component/login/Login'));
@@ -10,7 +11,7 @@ const MyRecord = lazy(() => import('./component/records/myrecord/MyRecord'));
 function App() {
   return (
     <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/review" element={<Review />} />
@@ -21,25 +22,5 @@ function App() {
     </Router>
   );
 }
-
-// import React, { Suspense } from 'react';
-
-// const OtherComponent = React.lazy(() => import('./OtherComponent'));
-
-// function MyComponent() {
-//   return (
-//     <div>
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <OtherComponent />
-//       </Suspense>
-//     </div>
-//   );
-// }
-
-// #469597
-// #5BA199
-// #BBC6C8
-// #E5E3E4
-// #DDBEAA
 
 export default App;
