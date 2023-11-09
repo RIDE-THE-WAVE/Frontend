a52655f6f9db3efff27cfd4626a64c454120ea16
jae-hwan-kim <woghks7859@gmail.com>
2023-11-09 15:06:24 +0900

    FEAT: 하위 컬렉션 데이터까지 불러오기 Test #16

--- ride-the-wave/src/Firebase/fetchData.js
+++ ride-the-wave/src/Firebase/fetchData.js
@@ -1,5 +1,5 @@
 import db from './Firebase';
-import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { setUserComments, setRecords, setUsers } from '../redux/action';
   
 // users 컬렉션에서 데이터 가져오기
@@ -8,13 +8,27 @@ export const fetchUsers = async (dispatch) => {
       const querySnapshot = await getDocs(collection(db, "users"));
       const users = [];
 
-      querySnapshot.forEach((doc) => {
-        users.push({
-          id: doc.id,
-          ...doc.data(),
+      for (const userDoc of querySnapshot.docs) {
+        const userData = userDoc.data();
+
+        const querySubSnapShot = await getDocs(collection(db, "users", userDoc.id, "records_display_option"));
+        const records_display_option = [];
+
+        querySubSnapShot.forEach((doc) => {
+          records_display_option.push({
+            id: doc.id,
+            ...doc.data(),
+          });
         });
-      });
-    dispatch(setUsers(users));
+
+        const user = {
+          id: userDoc.id,
+          ...userData,
+          "records_display_option": records_display_option,
+        };
+        users.push(user);
+      }
+      dispatch(setUsers(users));
   } catch (error) {
     console.error('Error fetching users:', error);
   }
@@ -26,12 +40,38 @@ export const fetchRecords = async (dispatch) => {
     const querySnapshot = await getDocs(collection(db, "records"));
     const records = [];
 
-    querySnapshot.forEach((doc) => {
-      records.push({
-        id: doc.id,
-        ...doc.data(),
+    for (const recordDoc of querySnapshot.docs) {
+      const recordData = recordDoc.data();
+
+      const queryFreestyleSnapShot = await getDocs(collection(db, "records", recordDoc.id, "freestyle"));
+      const freestyle = [];
+
+      queryFreestyleSnapShot.forEach((doc) => {
+        freestyle.push({
+          id: doc.id,
+          ...doc.data(),
+        });
       });
-    });
+
+      const queryBackstrokeSnapShot = await getDocs(collection(db, "records", recordDoc.id, "backstroke"));
+      const backstroke = [];
+
+      queryBackstrokeSnapShot.forEach((doc) => {
+        backstroke.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+
+
+      const record = {
+        id: recordDoc.id,
+        ...recordData,
+        "freestyle": freestyle,
+        "backstroke": backstroke,
+      };
+      records.push(record);
+    }
 
     dispatch(setRecords(records));
   } catch (error) {
