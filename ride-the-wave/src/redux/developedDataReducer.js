8addaef3ef29016378d247c75925bb4ba84526ed
jae-hwan-kim <woghks7859@gmail.com>
2023-11-14 23:05:20 +0900

    FIX: 리듀서 불변성 확보 #20

--- ride-the-wave/src/redux/developedDataReducer.js
+++ ride-the-wave/src/redux/developedDataReducer.js
@@ -1,10 +1,25 @@
 const initialState = {};
 
 const developedDataReducer = (state = initialState, action) => {
+
   switch (action.type) {
     case 'SET_DEVELOPED_DATA':
       return action.payload;
-    // Handle actions related to records here
+    case 'SET_CURRENT_USER_DATA':
+      return{
+        ...state,
+        current_user_data: action.payload,
+      };
+    case 'SET_CURRENT_USER':
+      return{
+        ...state,
+        current_user: action.payload,
+      };
+    case 'SET_AUTH':
+      return {
+        ...state,
+        auth: action.payload,
+      };
     default:
       return state;
   }
